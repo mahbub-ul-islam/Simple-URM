2be3e5675469ab3e48f1a5ccaf081ee2ea7574a2
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-09-24 22:02:46 +0600

    add email for authentication

--- grails-app/domain/com/learning/app/User.groovy
+++ grails-app/domain/com/learning/app/User.groovy
@@ -5,13 +5,13 @@ import groovy.transform.ToString
 import grails.compiler.GrailsCompileStatic
 
 @GrailsCompileStatic
-@EqualsAndHashCode(includes='username')
-@ToString(includes='username', includeNames=true, includePackage=false)
+@EqualsAndHashCode(includes='email')
+@ToString(includes='email', includeNames=true, includePackage=false)
 class User implements Serializable {
 
     private static final long serialVersionUID = 1
 
-    String username
+//    String username
     String email
     String password
     String firstName
@@ -30,7 +30,7 @@ class User implements Serializable {
 
     static constraints = {
         password nullable: false, blank: false, password: true
-        username nullable: false, blank: false, unique: true
+//        username nullable: false, blank: false, unique: true
         email nullable: false, blank: false, unique: true
         firstName nullable: false, blank: false
         lastName nullable: false, blank: false

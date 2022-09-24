6c363433e7600d5e4801e5bdd8c338d2ff5e6e05
Mahbub Ul Islam <mahbub_ul_islam@outlook.com>
2022-09-24 22:31:30 +0600

    login page added

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,5 +1,7 @@
+import com.learning.app.CustomUserDetailsService
 import com.learning.app.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
     userPasswordEncoderListener(UserPasswordEncoderListener)
+    userDetailsService(CustomUserDetailsService)
 }
